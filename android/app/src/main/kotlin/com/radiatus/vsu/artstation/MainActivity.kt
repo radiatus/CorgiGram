ce84c08a545e4bd8d2427de9dfcb5cb196c38e6e
radiatus <radiatus@yandex.ru>
2021-05-29 14:15:17 +0300

    Добавил пустой экран входа.Базовая настройка firebase

---
+++ android/app/src/main/kotlin/com/radiatus/vsu/artstation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.radiatus.vsu.artstation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/custom_router.dart
@@ -0,0 +1,37 @@
+import 'package:artstation/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+class CustomRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    print('Route: ${settings.name}');
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: '/'),
+          builder: (_) => const Scaffold(),
+        );
+      case SplashScreen.routeName:
+        return SplashScreen.route();
+      case LoginScreen.routeName:
+        return LoginScreen.route();
+      case NavScreen.routeName:
+        return NavScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: '/error'),
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('Something went wrong!'),
+        ),
+      ),
+    );
+  }
+}
